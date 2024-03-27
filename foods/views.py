a6ad12d018c74d45c43bea33f1df7ae69fc5ebe8
jongsung <jyou8304@gmail.com>
2024-03-27 14:18:35 -0400

    templates and view practice

--- foods/views.py
+++ foods/views.py
@@ -9,7 +9,10 @@ def index(request):
     return render(request,'foods/index.html', context=context)
 
 def food_detail(request,food):
-    context = {
-        "name" : food
-    }
+    context = dict()
+    if food == "chicken":
+        context['name'] = "코딩에 빠진 닭"
+        context['desc'] = "주머니가 가벼운 가격"
+        context['price'] = 20
+        context['img_path'] = "foods/images/chicken.jpg"
     return render(request, 'foods/detail.html', context = context)
