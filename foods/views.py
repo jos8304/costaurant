de64282a3fe32b2ae45d418c19afa58191569f1e
jongsung <jyou8304@gmail.com>
2024-03-27 09:29:49 -0400

    create dynamic index

--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return render(request,'foods/index.html')
+    today = datetime.today().date()
+    context = {
+        "date":today
+     }
+    return render(request,'foods/index.html', context=context)
 
